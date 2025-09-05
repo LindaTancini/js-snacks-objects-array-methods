8833fff743d77e4f59ab118ac8d0e18b66276482
LindaTancini <lindatancini95@gmail.com>
2025-09-05 10:48:36 +0200

    feat: complete filter, find and extra

--- script.js
+++ script.js
@@ -46,3 +46,108 @@ const users = [
 const map3 = users.map((e) => e.age);
 console.log(map3);
 console.log("----");
+
+// FILTER
+// Filtra solo quelli di tipo "cat".
+const animals = [
+  { name: "Jerry", type: "mouse" },
+  { name: "Tom", type: "cat" },
+  { name: "Garfield", type: "cat" },
+  { name: "Snoopy", type: "dog" },
+];
+const filter1 = animals.filter((e) => e.type === "cat");
+console.log(filter1);
+console.log("----");
+// Filtra solo i film con rating > 7.
+const movies = [
+  { title: "Inception", rating: 9 },
+  { title: "Avatar", rating: 7 },
+  { title: "Frozen", rating: 6 },
+  { title: "Interstellar", rating: 10 },
+];
+const filter2 = movies.filter((e) => e.rating > 7);
+console.log(filter2);
+console.log("----");
+// Filtra solo quelli con grade >= 18.
+const studentsGrades = [
+  { name: "Alessio", grade: 28 },
+  { name: "Martina", grade: 17 },
+  { name: "Giorgio", grade: 30 },
+  { name: "Francesca", grade: 15 },
+];
+const filter3 = studentsGrades.filter((e) => e.grade >= 18);
+console.log(filter3);
+console.log("----");
+
+// FIND
+//Trova la prima persona che vive a "Milano".
+const people = [
+  { name: "Sara", city: "Roma" },
+  { name: "Davide", city: "Milano" },
+  { name: "Elena", city: "Torino" },
+  { name: "Matteo", city: "Milano" },
+];
+const find1 = people.find((e) => e.city === "Milano");
+console.log(find1);
+console.log("----");
+// Trova il prodotto con id = 3.
+const productsById = [
+  { id: 1, name: "Tablet" },
+  { id: 2, name: "Smartphone" },
+  { id: 3, name: "Smartwatch" },
+  { id: 4, name: "Laptop" },
+];
+const find2 = productsById.find((e) => e.id === 3);
+console.log(find2);
+console.log("----");
+
+// LUNGHEZZA E RICERCA
+// Conta quanti studenti hanno un’età maggiore di 20.
+const studentsAges = [
+  { name: "Luca", age: 19 },
+  { name: "Marco", age: 21 },
+  { name: "Elisa", age: 23 },
+  { name: "Marta", age: 17 },
+];
+const extra1 = studentsAges.filter((e) => e.age > 20);
+console.log(extra1.length);
+console.log("----");
+
+//Verifica se esiste almeno un film con rating = 10.
+const movieRatings = [
+  { title: "Shrek", rating: 8 },
+  { title: "Titanic", rating: 9 },
+  { title: "The Godfather", rating: 10 },
+  { title: "Cars", rating: 7 },
+];
+const extra2 = movieRatings.some((e) => (e.rating = 10));
+console.log(extra2);
+console.log("----");
+// Trova la lunghezza dell’array e stampa: "La libreria contiene X libri".
+const booksCount = [
+  { title: "Dune", pages: 500 },
+  { title: "It", pages: 1200 },
+  { title: "Dracula", pages: 400 },
+];
+const extra3 = booksCount.length;
+console.log(`La libreria contiene ${extra3} libri`);
+console.log("----");
+// Trova il primo ordine con total > 100.
+const orders = [
+  { customer: "Alberto", total: 80 },
+  { customer: "Chiara", total: 120 },
+  { customer: "Gianni", total: 50 },
+  { customer: "Elisa", total: 200 },
+];
+const extra4 = orders.find((e) => e.total > 100);
+console.log(extra4);
+console.log("----");
+// Conta quante canzoni durano più di 3 minuti.
+const songs = [
+  { title: "Song A", duration: 180 },
+  { title: "Song B", duration: 240 },
+  { title: "Song C", duration: 200 },
+  { title: "Song D", duration: 300 },
+];
+const extra5 = songs.filter((e) => e.duration > 180);
+console.log(extra5.length);
