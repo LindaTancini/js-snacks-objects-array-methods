68f67433ee65ee28e5f0ce10ced068dd12a10d4d
LindaTancini <lindatancini95@gmail.com>
2025-09-25 10:40:53 +0200

    feat: complete 11 snacks

--- 17/script.js
+++ 17/script.js
@@ -48,16 +48,58 @@ const games = [
 ];
 
 // 1. (for) Stampare tutti i titoli dei videogiochi.
+for (let i = 0; i < games.length; i++) {
+  console.log("Titolo dei giochi:", games[i].title);
+}
+console.log("-------------------------------");
 // 2. (for) Calcolare il prezzo totale di tutti i videogiochi.
+let totale = 0;
+for (let i = 0; i < games.length; i++) {
+  totale += games[i].price;
+}
+console.log("Prezzo totale di tutti i giochi:", totale);
+console.log("-------------------------------");
 // 3. (for) Stampare i videogiochi pubblicati prima del 2016.
+for (let i = 0; i < games.length; i++) {
+  if (games[i].year < 2016) {
+    console.log("Giochi pubblicati prima del 2016:", games[i].title);
+  }
+}
+console.log("-------------------------------");
 // 4. (forEach) Stampare sviluppatore e anno di pubblicazione di ogni videogioco.
+games.forEach((e) =>
+  console.log(`Sviluppatore : ${e.developer} - Anno: ${e.year}`)
+);
+console.log("-------------------------------");
 // 5. (map) Creare un array con solo i prezzi dei videogiochi.
+const prezzi = games.map((e) => e.price);
+console.log("Prezzi dei giochi:", prezzi);
+console.log("-------------------------------");
 // 6. (filter) Ottenere tutti i videogiochi con rating >= 95.
+const ratingAlto = games.filter((e) => e.rating >= 95);
+console.log(ratingAlto);
+console.log("-------------------------------");
 // 7. (find) Trovare il primo videogioco con prezzo inferiore a 20€.
+const prezzoInferiore = games.find((e) => e.price < 20);
+console.log(prezzoInferiore);
+console.log("-------------------------------");
 // 8. (map) Creare un array di frasi tipo: "[title] sviluppato da [developer]".
+const frasi = games.map(
+  (e) => `${e.title} è stato sviluppato da ${e.developer}`
+);
+console.log(frasi);
+console.log("-------------------------------");
 // 9. (filter) Ottenere tutti i videogiochi del genere "RPG".
+const rpg = games.filter((e) => e.genre === "RPG");
+console.log(rpg);
+console.log("-------------------------------");
 // 10. (find) Trovare il videogioco intitolato "Minecraft".
+const minecraft = games.find((e) => e.title === "Minecraft");
+console.log(minecraft);
+console.log("-------------------------------");
 // 11. (forEach) Stampare titolo e rating di ogni videogioco.
+games.forEach((e) => console.log(`Titolo: ${e.title} - Rating : ${e.rating}`));
+console.log("-------------------------------");
 // 12. (map) Creare un array con gli anni di pubblicazione.
 // 13. (Accesso) Stampare il titolo del primo videogioco.
 // 14. (Accesso) Stampare il prezzo dell’ultimo videogioco.
